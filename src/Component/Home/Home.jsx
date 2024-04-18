1610f553364aa3f9b30a9160e292457c30dd7a04
momen210 <touristmomen@gmail.com>
2024-04-18 14:13:32 +0600

    banner added

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,9 +1,32 @@
+import Banner from "../../assets/banner.png";
 const Home = () => {
-    return (
-        <div>
-            
+  return (
+    <div>
+      <div
+        className="hero min-h-[80vh] bg-cover"
+        style={{
+          backgroundImage: `url(${Banner})`,
+          backgroundSize: "cover",
+        }}
+      >
+        <div className="hero-overlay bg-opacity-20"></div>
+        <div className="text-white max-w-2xl mr-auto ml-4 md:ml-12">
+          <div className="space-y-3">
+            <h1 className="text-2xl md:text-4xl font-bold flex-11">
+              Building Communities <br /> by Sharing Excess Food <br />{" "}
+              Resources
+            </h1>
+            <p>
+              Our mission is simple yet profound: to build resilient communities
+              by fostering the sharing of excess food resources. We believe that
+              in a world where millions go hungry while tons of food are wasted,
+              there is an urgent need for collective action
+            </p>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/MainLayout/MainLayout.jsx
+++ src/MainLayout/MainLayout.jsx
@@ -6,9 +6,7 @@ const MainLayout = () => {
   return (
     <div>
       <Navbar></Navbar>
-      <div className="max-w-7xl mx-auto">
-        <Outlet></Outlet>
-      </div>
+      <Outlet></Outlet>
       <Footer></Footer>
     </div>
   );
